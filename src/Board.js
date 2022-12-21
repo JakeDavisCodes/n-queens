e6c596c0a0b29ac68156feda95782ca1659f8da8
Jake <jakedavisemail@gmail.com>
2022-12-21 12:19:17 -0700

    Finished Board Functions

--- src/Board.js
+++ src/Board.js
@@ -62,7 +62,7 @@
     },
 
 
-/*
+    /*
          _             _     _
      ___| |_ __ _ _ __| |_  | |__   ___ _ __ ___ _
     / __| __/ _` | '__| __| | '_ \ / _ \ '__/ _ (_)
@@ -79,12 +79,28 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      let inRow = 0;
+      for (let i = 0; i < this.attributes[rowIndex].length; i++) {
+        if (this.attributes[rowIndex][i] === 1) {
+          inRow++;
+        }
+        if (inRow >= 2) {
+          return true;
+        }
+      } return false;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      // debugger;
+      for (let i = 0; i < this.attributes.n; i++) {
+        if (this.hasRowConflictAt(i)) {
+          return true;
+
+        }
+
+      }
+      return false;
     },
 
 
@@ -94,12 +110,24 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      let inCol = 0;
+      for (let i = 0; i < this.attributes.n; i++) {
+        if (this.attributes[i][colIndex] === 1) {
+          inCol++;
+        }
+        if (inCol >= 2) {
+          return true;
+        }
+      } return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      for (let i = 0; i < this.attributes.n; i++) {
+        if (this.hasColConflictAt(i)) {
+          return true;
+        }
+      } return false;
     },
 
 
@@ -108,13 +136,35 @@
     // --------------------------------------------------------------
     //
     // test if a specific major diagonal on this board contains a conflict
-    hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMajorDiagonalConflictAt: function(row, col) {
+      let inDiag = 0;
+      for (let i = 0; i < this.attributes.n; i++) {
+        if (row + i >= this.attributes.n || col + i >= this.attributes.n) {
+          return false;
+        }
+        if (this.attributes[row + i][col + i] === 1) {
+          inDiag++;
+        }
+        if (inDiag >= 2) {
+          return true;
+        }
+      } return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      for (let i = 0; i < this.attributes.n; i++) {
+        // debugger;
+        if (this.hasMajorDiagonalConflictAt(i, 0)) {
+          return true;
+        }
+      }
+      for (let i = 0; i < this.attributes.n; i++) {
+        // debugger;
+        if (this.hasMajorDiagonalConflictAt(0, i)) {
+          return true;
+        }
+      } return false;
     },
 
 
@@ -123,13 +173,35 @@
     // --------------------------------------------------------------
     //
     // test if a specific minor diagonal on this board contains a conflict
-    hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+    hasMinorDiagonalConflictAt: function(row, col) {
+      let inDiag = 0;
+      for (let i = 0; i < this.attributes.n; i++) {
+        if (row + i >= this.attributes.n || col + i < 0) {
+          return false;
+        }
+        if (this.attributes[row + i][col - i] === 1) {
+          inDiag++;
+        }
+        if (inDiag >= 2) {
+          return true;
+        }
+      } return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      for (let i = 0; i < this.attributes.n; i++) {
+        // debugger;
+        if (this.hasMinorDiagonalConflictAt(i, this.attributes.n - 1)) {
+          return true;
+        }
+      }
+      for (let i = 0; i < this.attributes.n; i++) {
+        // debugger;
+        if (this.hasMinorDiagonalConflictAt(0, i)) {
+          return true;
+        }
+      } return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
@@ -146,3 +218,4 @@
   };
 
 }());
+
